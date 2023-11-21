8a1435e12da68a40613cf8e2a7a9289631c1eaea
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-11-22 00:29:04 +0600

    Add
    Add

---
+++ code/crud-app/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Test_Crud_with_jason';
+}
