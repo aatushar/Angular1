b513621d867563091abb836f753e4eb424d48b02
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-11-30 09:25:50 +0600

    Add
    Add

--- code/SportStore/SportStore/src/app/store/store.module.ts
+++ code/SportStore/SportStore/src/app/store/store.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from "@angular/platform-browser";
  import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
  import { StoreComponent } from "./store.component";
+ 
 @NgModule({ imports: [ModelModule, BrowserModule, FormsModule], 
     declarations: [StoreComponent], exports: [StoreComponent] 
+    
 }) 
     export class StoreModule { }
